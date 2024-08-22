bf462cbb3dc8b5a5f966b46a399fd78f323bbd9d
fbx6094 <ggu6094@gmail.com>
2024-08-22 08:20:58 +0300

    as

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, render_template
 
 app = Flask(__name__)
 
@@ -10,7 +10,13 @@ def index():
         response = requests.get(f"https://parking.pythonanywhere.com/api/parking_history?phone_number={phone_number}")
         if response.status_code == 200:
             data = response.json()
-            html = """
+            # html = """
+            # return render_template('./templates/index.html', data=data)
+            # html = "index.html"
+            return render_template('index.html', data=response.json())
+        else:
+            return render_template('auth.html', data=response.json())
+    return render_template('auth.html')
 
 
 if __name__ == '__main__':
