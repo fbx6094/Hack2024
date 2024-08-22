b93e2a939a0256297565d58c55d57ae09a40ab8c
fbx6094 <ggu6094@gmail.com>
2024-08-22 11:34:42 +0300

    rob

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, request, render_template_string, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -10,14 +10,10 @@ def index():
         response = requests.get(f"https://parking.pythonanywhere.com/api/parking_history?phone_number={phone_number}")
         if response.status_code == 200:
             data = response.json()
-            # html = """
-            # return render_template('./templates/index.html', data=data)
-            # html = "index.html"
-            return render_template('index.html', data=response.json())
+            return render_template('index.html', data=data)
         else:
-            return render_template('auth.html', data=response.json())
+            return render_template('auth.html', error="Invalid phone number or API error")
     return render_template('auth.html')
 
-
-if __name__ == '__main__':  
-    app.run(debug=True) 
+if __name__ == '__main__':
+    app.run(debug=True)
